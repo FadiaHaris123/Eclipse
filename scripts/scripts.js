1c8b6ac0967269e4cffe3a4e4bb3f3197e8e898e
Fadia Haris <fadia.haris@experionglobal.com>
2022-11-05 12:09:14 +0530

    added about and contact

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -24,3 +24,68 @@ var swiper = new Swiper(".mySwiper",{
     },
     
 });
+function display(){
+  console.log("button clicked");
+
+}
+
+
+
+// --------------------------------------
+
+// -----------Contact--------------------
+
+
+const GTInput = document.querySelectorAll('.gt-input');
+
+  GTInput.forEach((check,idx) =>{
+
+    let currentLabel = GTInput[idx].parentElement.firstElementChild;
+    check.addEventListener('focus',() =>{
+
+      currentLabel.classList.add("move-up");
+
+        })
+
+    check.addEventListener('blur',() =>{
+
+      if(GTInput[idx].value === "") {
+
+        currentLabel.classList.remove("move-up");
+
+      }
+
+        })
+
+
+
+    })
+
+
+
+  let btnSend = document.querySelector('.gt-button');
+
+  let message = document.querySelector('.h1');
+
+
+
+  btnSend.addEventListener('click',() =>{
+
+    btnSend.innerText = 'Sending...';
+
+
+
+    setTimeout(()=>{
+
+
+
+      btnSend.style.display = "none";
+
+      message.innerText = "Message Sent";
+
+
+
+    },5000);
+
+  });
+
